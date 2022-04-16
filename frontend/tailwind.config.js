ff7b9ad85dd589b6494924c31d5a3fd29b5504b4
SeungmiNa <naissme0@gmail.com>
2022-04-15 18:42:17 -0700

    feat: set up eslint

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false,
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
