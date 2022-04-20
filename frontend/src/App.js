8321e3421858c73a6cff6497858936ecc9e99861
Seohyeon_Lee <sheee7@naver.com>
2022-04-20 17:43:07 +0900

    feat: add router for main page
    #34

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AdminMenu from './pages/AdminMenu.tsx';
+import Header from './components/Header.tsx';
+import Main from './pages/Main.tsx';
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <BrowserRouter>
         <h1>두게더의 딜러버스 팀!!</h1>
         <Routes>
-          {/* <Route path="/" element={Main} /> */}
-          <Route path="/admin/menu" element={AdminMenu} />
+          <Route path="/" element={<Main />} />
+          <Route path="/admin/menu" element={<AdminMenu />} />
         </Routes>
       </BrowserRouter>
     </div>
