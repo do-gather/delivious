7523a127309771f5980dd00da29158519cbcffe6
Seohyeon_Lee <sheee7@naver.com>
2022-04-21 23:40:27 +0900

    fix: make header link size

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <Link to='/'>
-            <img className="mx-14 py-9 w-28" alt='main-logo' src={Logo}></img>
-        </Link>);
+        <>
+            <div>
+                <Link className="w-28" to='/'>
+                    <img className="mx-14 py-9 w-28" alt='main-logo' src={Logo}></img>
+                </Link>
+            </div>
+        </>);
 }
