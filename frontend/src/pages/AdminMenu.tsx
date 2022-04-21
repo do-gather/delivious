013d6f458d1d0355f1b8f36bf87cf7ad938a038e
Seohyeon_Lee <sheee7@naver.com>
2022-04-21 23:40:45 +0900

    style: add admin menu text

--- frontend/src/pages/AdminMenu.tsx
+++ frontend/src/pages/AdminMenu.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function AdminMenu() {
-    return (<div className="w-full h-screen"></div>);
+    return (
+        <div className="w-full h-screen">
+            <div>메뉴관리 및 검색바</div>
+            <div className="flex mx-14 justify-between whitespace-nowrap text-white" style={{ backgroundColor: "#435CA5" }}>
+                <div>Category</div>
+                <div>ID</div>
+                <div>Name</div>
+                <div>Ice/Hot</div>
+                <div>Size</div>
+                <div>Price</div>
+                <div>Date</div>
+            </div>
+        </div>
+    );
 }
