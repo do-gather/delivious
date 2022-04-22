0a2f49ac3fa23e7da1b26ba510d708f15ec182db
Seohyeon_Lee <sheee7@naver.com>
2022-04-22 23:43:20 +0900

    feat: add admin menu item

---
+++ frontend/src/components/AdminMenuItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function AdminMenuItem() {
+    return (
+        <>
+            <div className="flex px-10 py-6 justify-between whitespace-nowrap text-center items-center text-base">
+                <div>Category</div>
+                <div>ID</div>
+                <div>Name</div>
+                <div>Ice/Hot</div>
+                <div>Size</div>
+                <div>Price</div>
+                <div>Date</div>
+            </div>
+        </>);
+}
