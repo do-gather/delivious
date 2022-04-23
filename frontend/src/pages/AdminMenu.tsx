358a2a62a64b7967e055450bb0c5e123f73b58e8
Seohyeon_Lee <sheee7@naver.com>
2022-04-23 19:01:40 +0900

    feat: add search box on admin menu page

--- frontend/src/pages/AdminMenu.tsx
+++ frontend/src/pages/AdminMenu.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AdminMenuItem from "../components/AdminMenuItem.tsx";
 import constants from "../utils/constants.ts";
 import EditButton from "../images/EditButton.tsx";
+import SearchBox from "../components/SearchBox.tsx";
 import SideButton from "../components/SideButton.tsx";
 import { Link } from 'react-router-dom';
 
@@ -18,8 +19,11 @@ function Divider() {
 
 export default function AdminMenu() {
     return (
-        <div className="w-full h-screen px-14">
-            <div className="font-bold text-3xl pb-9">메뉴관리</div>
+        <div className="w-full h-screen pl-14 pr-32">
+            <div className="flex space-x-24">
+                <div className="font-bold text-3xl pb-9">메뉴관리</div>
+                <SearchBox placeholder="Search menu name or id" />
+            </div>
             <div className="grid grid-cols-7 gap-4 px-10 justify-between font-semibold whitespace-nowrap text-white h-12 text-center items-center text-2xl" style={{ backgroundColor: "#435CA5" }}>
                 <div>Category</div>
                 <div>ID</div>
