989b9ef23753e7a89a8707a5ef67dc17186305ae
Seohyeon_Lee <sheee7@naver.com>
2022-04-23 18:00:10 +0900

    fix: change flex to grid

--- frontend/src/components/AdminMenuItem.tsx
+++ frontend/src/components/AdminMenuItem.tsx
@@ -13,7 +13,7 @@ interface Props {
 export default function AdminMenuItem(props: Props) {
     return (
         <>
-            <div className="flex px-16 py-6 justify-between whitespace-nowrap text-center items-center text-base">
+            <div className="grid grid-cols-7 px-10 gap-4 py-6 justify-between whitespace-nowrap text-center items-center text-base">
                 <div>{props.category}</div>
                 <div>{props.id}</div>
                 <div>{props.name}</div>
--- frontend/src/pages/AdminMenu.tsx
+++ frontend/src/pages/AdminMenu.tsx
@@ -11,7 +11,7 @@ export default function AdminMenu() {
     return (
         <div className="w-full h-screen px-14">
             <div className="font-bold text-3xl pb-9">메뉴관리</div>
-            <div className="flex px-10 justify-between font-semibold whitespace-nowrap text-white h-12 text-center items-center text-2xl" style={{ backgroundColor: "#435CA5" }}>
+            <div className="grid grid-cols-7 gap-4 px-10 justify-between font-semibold whitespace-nowrap text-white h-12 text-center items-center text-2xl" style={{ backgroundColor: "#435CA5" }}>
                 <div>Category</div>
                 <div>ID</div>
                 <div>Name</div>
