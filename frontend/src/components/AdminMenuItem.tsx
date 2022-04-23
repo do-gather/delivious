03bdc46b7e4674e4fa42c0bd6a0d039063f5409d
Seohyeon_Lee <sheee7@naver.com>
2022-04-23 19:01:48 +0900

    chore: edit lines

--- frontend/src/components/AdminMenuItem.tsx
+++ frontend/src/components/AdminMenuItem.tsx
@@ -19,15 +19,14 @@ interface Props {
 
 export default function AdminMenuItem({ props }: Props) {
     return (
-        <>
-            <div className="grid grid-cols-7 px-10 gap-4 py-6 justify-between whitespace-nowrap text-center items-center text-base">
-                <div>{props.category}</div>
-                <div>{props.id}</div>
-                <div>{props.name}</div>
-                <div>{props.temp}</div>
-                <div>{props.size}</div>
-                <div>{props.price}</div>
-                <div>{props.date}</div>
-            </div>
-        </>);
+        <div className="grid grid-cols-7 px-10 gap-4 py-6 justify-between whitespace-nowrap text-center items-center text-base">
+            <div>{props.category}</div>
+            <div>{props.id}</div>
+            <div>{props.name}</div>
+            <div>{props.temp}</div>
+            <div>{props.size}</div>
+            <div>{props.price}</div>
+            <div>{props.date}</div>
+        </div>
+    );
 }
