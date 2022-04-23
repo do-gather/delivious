32a5c48e6597d6184926fe46be49d17281f8903d
Seohyeon_Lee <sheee7@naver.com>
2022-04-23 18:11:04 +0900

    feat: add constants file for test data

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,23 @@
+const constants = {
+    MENU_ITEMS: [
+        {
+            category: 'Coffee',
+            id: '1391039',
+            name: '아이스 아메리카노',
+            temp: 'Ice',
+            size: 'Tall',
+            price: '4,000',
+            date: "2021.11.30"
+        },
+        {
+            category: 'Non-Coffee',
+            id: '1414612',
+            name: '초코 라떼',
+            temp: 'Ice, Hot',
+            size: 'Tall, Grande',
+            price: '5,000',
+            date: "2021.1.30"
+        },
+    ]
+}
+export default constants;
