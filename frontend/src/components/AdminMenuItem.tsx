a506a45f0c3a5f22398bad5f71e8ec732d614b73
Seohyeon_Lee <sheee7@naver.com>
2022-04-27 01:13:43 +0900

    feat: add check box input items on edit page

--- frontend/src/components/AdminMenuItem.tsx
+++ frontend/src/components/AdminMenuItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CheckBox from "./CheckBox.tsx";
 import EditBox from "./EditBox.tsx";
 
 /**
@@ -19,13 +20,25 @@ interface Props {
 }
 
 export default function AdminMenuItem({ props, type = "" }: Props) {
+
     return (
         <div className="grid grid-cols-7 px-10 gap-4 py-6 justify-between whitespace-nowrap text-center items-center text-base">
             {type === 'edit' ? <EditBox placeholder="카테고리" text={props.category} /> : <div>{props.category}</div>}
             <div>{props.id}</div>
             {type === 'edit' ? <EditBox placeholder="메뉴 이름" text={props.name} /> : <div>{props.name}</div>}
-            <div>{props.temp}</div>
-            <div>{props.size}</div>
+            {type === 'edit' ?
+                <div className="flex justify-center space-x-3">
+                    <CheckBox label="Hot" checked={true} />
+                    <CheckBox label="Ice" checked={true} />
+                </div> :
+                <div>{props.temp}</div>}
+            {type === 'edit' ?
+                <div className="flex-col justify-center space-y-1">
+                    <CheckBox label="Tall" checked={true} />
+                    <CheckBox label="Grande" checked={true} />
+                    <CheckBox label="Venti" checked={true} />
+                </div> :
+                <div>{props.size}</div>}
             {type === 'edit' ? <EditBox placeholder="가격" text={props.price} /> : <div>{props.price}</div>}
             <div>{props.date}</div>
         </div>
