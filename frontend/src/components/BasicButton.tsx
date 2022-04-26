c71ce7eef00def21860332d50eab25be951b3c4c
Seohyeon_Lee <sheee7@naver.com>
2022-04-27 00:04:52 +0900

    feat: add parameter for button

--- frontend/src/components/BasicButton.tsx
+++ frontend/src/components/BasicButton.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 /**
- * 검색 입력 창
- * @param placeholder 플레이스 홀더에 들어갈 말을 변수로 받음
+ * 기본 직사각형 버튼
+ * @param buttonName 버튼에 들어갈 이름을 받음
  */
 interface Props {
-    placeholder: string;
+    buttonName: string;
 }
 
 export default function BasicButton(props: Props) {
     return (
-        <div className='rounded text-sm text-center py-2.5 object-left-top h-10' style={{ backgroundColor: '#435ca5' }}>
-            편집 완료
+        <div className='flex items-center rounded text-base font-bold text-white text-center px-10 object-left-top h-10' style={{ backgroundColor: '#435ca5' }}>
+            {props.buttonName}
         </div>
     );
 }
