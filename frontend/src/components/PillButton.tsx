134423934002e9684147c15aa3a15e91b6b01b6a
Seohyeon_Lee <sheee7@naver.com>
2022-04-30 01:46:56 +0900

    feat: set active effect on pill button

---
+++ frontend/src/components/PillButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+/**
+ * 기본 둥근 모서리 버튼 (메뉴 담는 과정에서 쓰임)
+ * @param buttonTitle 버튼에 들어갈 이름을 받음
+ * @param onClick 버튼 클릭 시 실행될 함수 받음
+ * @param activeColor 버튼 클릭 시 바뀔 배경 색상 받음
+ */
+interface Props {
+    buttonTitle: string;
+    onClick: any;
+    activeColor?: string;
+}
+
+export default function PillButton({ buttonTitle, onClick, activeColor = "active:bg-indigo-900" }: Props) {
+
+    return (
+        <div className={`flex whitespace-nowrap justify-center items-center rounded-3xl text-base text-center px-2 h-7 cursor-pointer 
+        bg-white ${activeColor} active:text-white text-black `}
+            onClick={onClick}>
+            {buttonTitle}
+        </div>
+    );
+}
