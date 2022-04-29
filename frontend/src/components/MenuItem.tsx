278bbe58ea4ba12bda3d529f56d20a7a6ef64f61
Seohyeon_Lee <sheee7@naver.com>
2022-04-30 01:47:23 +0900

    feat: make steps on menu item selection process

--- frontend/src/components/MenuItem.tsx
+++ frontend/src/components/MenuItem.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import getYmd10 from "../utils/utils.tsx";
 import AddCheck from "../images/AddCheck.tsx";
-import MenuButton from "./MenuButton.tsx";
+import PillButton from "./PillButton.tsx";
 
 /**
  * 사용자 메뉴 목록의 각 아이템
  * 
  * @param props 메뉴안에 들어갈 각 항목을 아래 인터페이스 형태로 묶어서 받음
- * @param type 아이템이 수정 상태인지 아닌지 입력, 기본은 'display'. 온도 선택 시 'temp'을 전달. 사이즈 선택 시 'size' 전달. 최종 선택 시 'add' 전달. 전달하지 않으면 'display'로 초기화 됨
- */
+*/
 
 interface Props {
     props?: {
@@ -20,10 +19,9 @@ interface Props {
         price: string;
         date: string;
     }
-    type?: string;
 }
 
-export default function MenuItem({ props, type = 'display' }: Props) {
+export default function MenuItem({ props }: Props) {
     const [currentDate, setCurrentDate] = useState(getYmd10())
     const [menu, setMenu] = useState(props ? props : props = {
         category: '',
@@ -34,28 +32,41 @@ export default function MenuItem({ props, type = 'display' }: Props) {
         price: '',
         date: '',
     })
+    const [step, setStep] = useState(0)
 
-    const handleChecked = (keyword: string, label: string) => {
+    // 메뉴에 있는 옵션만 화면에 보여주기 위한 함수
+    const handleCheckLabel = (keyword: string, label: string) => {
         return keyword.includes(label)
     };
 
+    const handleClicked = () => {
+        if (step === 3)
+            setStep(0)
+        else
+            setStep(step + 1)
+    }
+
     return (
         <div className="text-center text-xs w-36">
-            <div className={`w-36 h-36 bg-white whitespace-nowrap items-center ${type === "display" && "cursor-pointer"} bg-contain bg-center`}
-                style={{ backgroundImage: "url(https://myauto.shinhancard.com/conts/images/event/evt_181001_02_gift04.png)" }} >
-                {type === "temp" && <div className="flex justify-center items-center space-x-2 bg-zinc-500/50 w-full h-full">
-                    <MenuButton buttonName="Hot" textColor="text-black" bgColor="#ffffff" />
-                    <MenuButton buttonName="Ice" textColor="text-black" bgColor="#ffffff" />
+            {step === 0 ? <div className={`w-36 h-36 bg-white whitespace-nowrap items-center cursor-pointer bg-contain bg-center`}
+                style={{ backgroundImage: "url(https://myauto.shinhancard.com/conts/images/event/evt_181001_02_gift04.png)" }}
+                onClick={handleClicked}> </div> :
+                <div className={`w-36 h-36 bg-white whitespace-nowrap items-center bg-contain bg-center`}
+                    style={{ backgroundImage: "url(https://myauto.shinhancard.com/conts/images/event/evt_181001_02_gift04.png)" }}
+                >
+                    {step === 1 && <div className="flex justify-center items-center space-x-2 bg-zinc-500/50 w-full h-full" >
+                        {handleCheckLabel(props.temp, "Hot") && <PillButton buttonTitle="Hot" activeColor="active:bg-rose-900" onClick={handleClicked} />}
+                        {handleCheckLabel(props.temp, "Ice") && <PillButton buttonTitle="Ice" activeColor="active:bg-indigo-800" onClick={handleClicked} />}
+                    </div>}
+                    {step === 2 && <div className="flex flex-col justify-center items-center space-y-3.5 bg-zinc-500/50 w-full h-full">
+                        {handleCheckLabel(props.size, "Tall") && <PillButton buttonTitle="Tall" onClick={handleClicked} />}
+                        {handleCheckLabel(props.size, "Grande") && <PillButton buttonTitle="Grande" onClick={handleClicked} />}
+                        {handleCheckLabel(props.size, "Venti") && <PillButton buttonTitle="Venti" onClick={handleClicked} />}
+                    </div>}
+                    {step === 3 && <div className="flex justify-center items-center bg-zinc-500/50 w-full h-full" onClick={handleClicked}>
+                        <AddCheck />
+                    </div>}
                 </div>}
-                {type === "size" && <div className="flex flex-col justify-center items-center space-y-3.5 bg-zinc-500/50 w-full h-full">
-                    <MenuButton buttonName="Tall" textColor="text-black" bgColor="#ffffff" />
-                    <MenuButton buttonName="Grande" textColor="text-black" bgColor="#ffffff" />
-                    <MenuButton buttonName="Venti" textColor="text-black" bgColor="#ffffff" />
-                </div>}
-                {type === "add" && <div className="flex justify-center items-center bg-zinc-500/50 w-full h-full">
-                    <AddCheck />
-                </div>}
-            </div>
             <div className="mt-3">{props.name}</div>
         </div >
     );
