fa008e73ea0ae22dd8612149f9e3933ec355755b
Seohyeon_Lee <sheee7@naver.com>
2022-05-04 22:41:52 +0900

    chore: make image tsx files for admin main page

---
+++ frontend/src/images/Copy.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+function Copy() {
+  return (
+    <svg width="125" height="126" viewBox="0 0 125 126" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M78.125 78.75h26.042c2.876 0 5.208-2.35 5.208-5.25V21c0-2.9-2.332-5.25-5.208-5.25H52.083c-2.876 0-5.208 2.35-5.208 5.25v26.25"
+        stroke="#000"
+        strokeWidth={4}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.833 110.25h52.084c2.876 0 5.208-2.351 5.208-5.25V52.5c0-2.9-2.332-5.25-5.208-5.25H20.833c-2.876 0-5.208 2.35-5.208 5.25V105c0 2.899 2.332 5.25 5.208 5.25Z"
+        stroke="#000"
+        strokeWidth={4}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default Copy;
---
+++ frontend/src/images/Notebook.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+function Notebook() {
+  return (
+    <svg width="132" height="130" viewBox="0 0 132 130" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M104.5 21.667h-77c-3.038 0-5.5 2.425-5.5 5.416v75.834c0 2.991 2.462 5.416 5.5 5.416h77c3.038 0 5.5-2.425 5.5-5.416V27.083c0-2.991-2.462-5.416-5.5-5.416ZM66 59.583h22M66 43.333h22M44 108.333V21.667"
+        stroke="#000"
+        strokeWidth={4}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default Notebook;
