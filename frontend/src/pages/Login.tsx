5466caf4558c564c9402cbbff4720f4e966b76de
SeungmiNa <naissme0@gmail.com>
2022-05-07 19:15:38 -0700

    feat: make login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,28 @@
+import React from 'react';
+import BasicButton from '../components/BasicButton';
+import MainLogo from '../images/MainLogo';
+
+/**
+ * '/ligin'으로 연결되는 로그인 페이지
+ */
+
 export default function Login() {
-  // eslint-disable-next-line no-useless-return
-  return;
+  return (
+    <div>
+      <div className='flex justify-center'>
+        <div className="flex justify-center mt-64">
+          <MainLogo width='576' height='122.016'/>
+        </div>
+      </div>
+      
+      <div className='flex justify-center'>
+        <div className="flex justify-center text-3xl font-bold" style={{paddingTop:"1.373rem"}} >관리자 로그인</div>
+      </div>
+      <div className='flex justify-center'>
+        <div>
+          <BasicButton buttonName='Log in'/>
+        </div>
+      </div>
+    </div>
+  );
 }
