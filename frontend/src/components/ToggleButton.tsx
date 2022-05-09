ecf6856757ccc908bbfaf29d0410c45857e4dfc5
Seohyeon_Lee <sheee7@naver.com>
2022-05-10 00:10:12 +0900

    feat: add toggle button component for take out option selection
    #47

---
+++ frontend/src/components/ToggleButton.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/require-default-props */
+import React, { useState } from 'react';
+
+/**
+ * 직사각형 토글 버튼
+ * @param option1 첫 번째 토글 버튼에 들어갈 이름을 받음
+ * @param option2 두 번째 토글 버튼에 들어갈 이름을 받음
+ * @param clickedOption 선택된 토글 버튼의 순서를 숫자로 받음
+ */
+interface Props {
+  option1: string;
+  option2: string;
+  clickedOption: number;
+}
+
+export default function ToggleButton({ option1, option2, clickedOption }: Props) {
+  const [clicked, setClicked] = useState(clickedOption);
+  return (
+    <div className="flex -space-x-1">
+      <button
+        type="button"
+        className={`flex whitespace-nowrap items-center rounded-l text-base font-bold text-white text-center 
+        ${clicked === 1 && 'rounded-r z-50'} px-10 object-left-top h-10`}
+        style={{ backgroundColor: clicked === 1 ? '#000000' : '#c4c4c4' }}
+        onClick={() => {
+          setClicked(1);
+        }}
+      >
+        {option1}
+      </button>
+      <button
+        type="button"
+        className={`flex whitespace-nowrap items-center  rounded-r text-base font-bold text-white text-center 
+        ${clicked === 2 && 'rounded-l'}  px-10 object-left-top h-10`}
+        style={{ backgroundColor: clicked === 2 ? '#000000' : '#c4c4c4' }}
+        onClick={() => {
+          setClicked(2);
+        }}
+      >
+        {option2}
+      </button>
+    </div>
+  );
+}
