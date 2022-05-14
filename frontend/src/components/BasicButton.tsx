3fc960199ead770be09ea0774b5b0b172903e309
SeungmiNa <naissme0@gmail.com>
2022-05-14 14:21:56 -0700

    fix: add height param and icon location

--- frontend/src/components/BasicButton.tsx
+++ frontend/src/components/BasicButton.tsx
@@ -6,19 +6,21 @@ import React from 'react';
  * @param buttonName 버튼에 들어갈 이름을 받음
  * @param onClick 버튼이 눌렸을 때 작동할 함수 받음
  * @param xPadding 버튼 안쪽의 좌우 패딩을 변수로 받아 셋팅, 기본은 px-10
+ * @param height 버튼의 높이를 받음, 기본은 h-10
  */
 interface Props {
   buttonName: string;
   onClick: any;
   xPadding?: string;
+  height?: string;
 }
 
-export default function BasicButton({ buttonName, onClick, xPadding = 'px-10' }: Props) {
+export default function BasicButton({ buttonName, onClick, xPadding = 'px-10', height = 'h-10' }: Props) {
   return (
     <button
       type="button"
-      className={`flex whitespace-nowrap items-center rounded text-base font-bold text-white text-center 
-        ${xPadding} object-left-top h-10`}
+      className={`whitespace-nowrap items-center rounded text-base font-bold text-white text-center 
+        ${xPadding} object-left-top ${height}`}
       style={{ backgroundColor: '#435ca5' }}
       onClick={onClick}
     >
--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -7,15 +7,20 @@ import React, { useState } from 'react';
  * @param text input box에 채워져 있어야 할 텍스트를 변수로 받음, 기본은 ''
  * @param icon input box에 아이콘이 있다면 아이콘을 받음
  * @param mode input box 색상 테마를 받음 기본은 normal, 어두운 것은 dark
+ * @param height input box 높이를 받음.기본은 h-10
  */
 interface Props {
   placeholder: string;
   text?: string;
   icon?: any;
+  iconLeft?: boolean;
   mode?: string;
+  height?: string;
 }
 
-export default function InputBox({ placeholder, icon = 'none', text = '', mode = 'normal' }: Props) {
+
+export default function InputBox({ placeholder, 
+  iconLeft=false, height='h-10', icon = 'none', text = '', mode = 'normal' }: Props) {
   const [value, setValue] = useState(text);
 
   const handleTextChange = (e: any) => {
@@ -28,18 +33,22 @@ export default function InputBox({ placeholder, icon = 'none', text = '', mode =
         <input
           className={
             mode === 'dark'
-              ? 'placeholder-gray-400 text-white bg-black h-9 w-full my-1 px-2 rounded'
-              : 'bg-white text-base text-center placeholder-gray-500 py-2.5 h-10 w-36'
+              ? `placeholder-gray-400 text-white ${height} bg-black w-full my-1 px-2 rounded`
+              : "bg-white text-base text-center placeholder-gray-500 py-2.5 h-10 w-36"
           }
           placeholder={placeholder}
           value={value}
           onChange={handleTextChange}
         />
       ) : (
-        <div className="relative my-1">
-          <div className="absolute right-2 top-2.5">{icon}</div>
+       
+        <div className="relative my-1 flex justify-center align-middle">
+          <div className={`absolute 
+            ${iconLeft 
+              ? 'left-2 top-5': 'right-2 top-3.5'}`}> {icon} </div>
           <input
-            className="pl-2 pr-7 h-9 w-full rounded placeholder-gray-400 text-white bg-black  object-left-top"
+            className={`pr-7 ${height} w-full rounded placeholder-gray-400 text-white bg-black  object-left-top 
+              ${iconLeft ? 'pl-16': 'pl-2'}`}
             placeholder={placeholder}
             value={value}
             onChange={handleTextChange}
