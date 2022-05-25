b3fb29fe5685517190c8c2cf7a0acc7dc6c603a8
SeungmiNa <naissme0@gmail.com>
2022-05-24 18:40:15 -0700

    chore: add param IconLeft

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -6,6 +6,7 @@ import React, { useState } from 'react';
  * @param placeholder 플레이스 홀더에 들어갈 말을 변수로 받음
  * @param text input box에 채워져 있어야 할 텍스트를 변수로 받음, 기본은 ''
  * @param icon input box에 아이콘이 있다면 아이콘을 받음
+ * @param iconLeft input box에 아이콘이 왼쪽에 있는경우를 받음
  * @param mode input box 색상 테마를 받음 기본은 normal, 어두운 것은 dark
  * @param height input box 높이를 받음.기본은 h-10
  */
@@ -33,7 +34,7 @@ export default function InputBox({ placeholder,
         <input
           className={
             mode === 'dark'
-              ? `placeholder-gray-400 text-white ${height} bg-black w-full my-1 px-2 rounded`
+              ? `placeholder-gray-400 text-white ${height} bg-black w-full px-2 rounded`
               : "bg-white text-base text-center placeholder-gray-500 py-2.5 h-10 w-36"
           }
           placeholder={placeholder}
@@ -42,7 +43,7 @@ export default function InputBox({ placeholder,
         />
       ) : (
        
-        <div className="relative my-1 flex justify-center align-middle">
+        <div className="relative flex justify-center align-middle">
           <div className={`absolute 
             ${iconLeft 
               ? 'left-2 top-5': 'right-2 top-3.5'}`}> {icon} </div>
