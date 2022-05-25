b3914de6b807cd447edc7af7b18a6ade9485ed2c
SeungmiNa <naissme0@gmail.com>
2022-05-24 18:45:01 -0700

    style: change duplicated code to simple code with space padding

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import BasicButton from '../components/BasicButton';
 import InputBox from '../components/InputBox';
-import IdIcon from '../images/IdIcon';
+import UserIcon from '../images/UserIcon';
 import PasswordIcon from '../images/PasswordIcon';
 
 /**
@@ -10,32 +10,23 @@ import PasswordIcon from '../images/PasswordIcon';
  */
 
 export default function Login() {
+
   return (
-    <div className="justify-center" style={{marginTop:"25.125rem"}}>      
-      <div className="flex justify-center text-3xl font-bold" style={{paddingTop:"1.373rem"}} >
-          관리자 로그인
+    <div className="flex-col justify-center" style={{marginTop:"25.125rem"}}>
+      <div className="w-full text-center text-3xl font-bold" style={{paddingTop:"1.373rem"}}>
+        관리자 로그인
       </div>
-      <div className="flex justify-center" style={{ marginTop:"4.125rem"}}>
-        <div style={{width:"30rem"}}>
-          <div style={{paddingBottom:"0.51rem"}}>
-            <InputBox placeholder="ID" icon={<IdIcon/>} iconLeft height="h-16" />
-          </div>
-          <div>
-            <InputBox placeholder="Password" height="h-16" icon={<PasswordIcon/>} iconLeft/>
-          </div>
-          <div className="flex justify-center mt-8" style={{ fontSize:"0.875rem"}}>
-            <div style={{paddingRight:"3.281rem"}}>
-              <Link to="/sign-up">Create Account</Link>
-            </div>
-            <div style={{paddingLeft:"3.281rem"}}>
-              <Link to="/">Contact Delivious</Link>
-            </div>
+      <div className="flex justify-center pt-16">
+        <div className="flex-col space-y-4 pr-4" style={{width:"30rem"}}>
+          <InputBox placeholder="ID" icon={<UserIcon/>} iconLeft height="h-16" />
+          <InputBox placeholder="Password" height="h-16" icon={<PasswordIcon/>} iconLeft/>
+          <div className="flex justify-center space-x-32 pt-1.5"  style={{ fontSize:"0.875rem"}}>
+            <Link to="/sign-up">Create Account</Link>
+            <Link to="/">Contact Delivious</Link>
           </div>
         </div>
-        <div style={{width:"6.188rem", marginLeft:"0.938rem", marginTop:"0.25rem"}}>
-          <BasicButton buttonName="Log in" onClick={()=>{console.log("click")}} height="h-36"/>
-        </div>
+        <BasicButton buttonName="Log in" onClick={()=>{console.log("click")}} height="h-36"/>
       </div>
     </div>
-  );
+  )
 }
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import BasicButton from "../components/BasicButton";
 import InputBox from "../components/InputBox";
 import CheckBox from "../components/CheckBox";
-import IdIcon from "../images/IdIcon";
+import UserIcon from "../images/UserIcon";
 import PasswordIcon from "../images/PasswordIcon";
 
 /**
@@ -13,48 +13,47 @@ export default function SignUp() {
   const [admin, setAdmin] = useState(false);
   
   return(
-    <div>
-      <div className="flex justify-center">
-        <div style={{ marginTop:"3.75rem", width:"30.062rem"}}>
-          <div style={{ marginTop:"3.566rem", paddingBottom:"0.562rem"}}>
-              <div className="text-base font-bold">아이디</div>
-              <InputBox placeholder="ID" height="h-14" mode="dark" icon={<IdIcon/>}/>
+    <div className="flex justify-center mt-36">
+      <div className="flex-col space-y-4">
+        <div>
+          <div className="text-base font-bold">아이디</div>
+          <InputBox placeholder="ID" height="h-14" mode="dark" icon={<UserIcon/>}/>
+        </div>
+        <div>
+          <div className="text-base font-bold">비밀번호</div>
+          <InputBox placeholder="Password" height="h-14" mode="dark" icon={<PasswordIcon/>}/>
+        </div>
+        <div>
+          <div className="text-base font-bold">비밀번호 재확인</div>
+          <InputBox placeholder="Password Confirm" height="h-14" mode="dark"/>
+        </div>
+        <div>
+          <div className="text-base font-bold">이름</div>
+          <InputBox placeholder="Name" height="h-14" mode="dark"/>
+        </div>
+        <div>
+          <div className="text-base font-bold">생년월일</div>
+          <div className="flex justify-center space-x-2">
+            <InputBox placeholder="Year" height="h-14" mode="dark"/>
+            <InputBox placeholder="Month" height="h-14" mode="dark"/>
+            <InputBox placeholder="Date" height="h-14" mode="dark"/>
           </div>
-          <div style={{ marginTop:"0.5rem", paddingBottom:"0.562rem"}}>
-              <div className="text-base font-bold">비밀번호</div>
-              <InputBox placeholder="Password" height="h-14" mode="dark" icon={<PasswordIcon/>}/>
-          </div>
-          <div style={{ marginTop:"0.5rem", paddingBottom:"0.562rem"}}>
-              <div className="text-base font-bold">비밀번호 재확인</div>
-              <InputBox placeholder="Password Confirm" height="h-14" mode="dark"/>
-          </div>
-          <div style={{ marginTop:"0.5rem", paddingBottom:"0.562rem"}}>
-              <div className="text-base font-bold">이름</div>
-              <InputBox placeholder="Name" height="h-14" mode="dark"/>
-          </div>
-          <div style={{ marginTop:"0.5rem", paddingBottom:"0.562rem"}}>
-              <div className="text-base font-bold">생년월일</div>
-              <div className="flex justify-center space-x-2">
-                <InputBox placeholder="Year" height="h-14" mode="dark"/>
-                <InputBox placeholder="Month" height="h-14" mode="dark"/>
-                <InputBox placeholder="Date" height="h-14" mode="dark"/>
-              </div>
-          </div>
-          <div style={{ marginTop:"0.5rem", paddingBottom:"0.562rem"}}>
-              <div className="text-base font-bold">휴대전화</div>
-              <InputBox placeholder="Phone Number" height="h-14" mode="dark"/>
-          </div>
-          <button type="button" style={{ marginTop:"0.5rem", paddingBottom:"0.562rem"}} 
-            className="text-base font-bold" onClick={()=>setAdmin(!admin)}>
-              <CheckBox label="관리자 회원가입" checked={admin}/>
+        </div>
+        <div>
+          <div className="text-base font-bold">휴대전화</div>
+          <InputBox placeholder="Phone Number" height="h-14" mode="dark"/>
+        </div>
+        <div>
+          <button type="button" className="text-base font-bold" onClick={()=>setAdmin(!admin)}>
+            <CheckBox label="관리자 회원가입" checked={admin}/>
           </button>
-          {admin && <div style={{ marginTop:"0.5rem", paddingBottom:"0.562rem"}}>
-              <div className="text-base font-bold">매장명</div>
-              <InputBox placeholder="Store Name" height="h-14"mode="dark"/>
+          {admin && <div>
+            <div className="text-base font-bold">매장명</div>
+            <InputBox placeholder="Store Name" height="h-14"mode="dark"/>
           </div>}
-          <div style={{ marginTop:"0.5rem", paddingBottom:"0.562rem", width:"30.062rem", display:"center"}}>
-              <BasicButton buttonName="Sign Up" onClick={()=>{console.log("signup")}} height="h-14" xPadding="w-full" />
-          </div>
+        </div>
+        <div>
+          <BasicButton buttonName="Sign Up" onClick={()=>{console.log("signup")}} height="h-14" xPadding="w-full" />
         </div>
       </div>
     </div>
