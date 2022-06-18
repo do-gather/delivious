ca984b70eb201c7ec8d15a36124bd5ed89bf04e4
hyelin <k1h2fls@gmail.com>
2022-06-18 22:25:45 +0900

    test

--- backend/src/main/java/com/delivious/backend/domain/users/config/SecurityConfig.java
+++ backend/src/main/java/com/delivious/backend/domain/users/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         ,"/categories/**"
                         ,"/menus/**"
 
+                        ,"/api/hello"
                 );
     }
 
