0a9a4033768bd7ebf1384eff0ba4c4409bc2884c
hyelin <k1h2fls@gmail.com>
2022-06-18 22:25:45 +0900

    test

--- backend/src/main/java/com/delivious/backend/domain/menu/entity/Menu.java
+++ backend/src/main/java/com/delivious/backend/domain/menu/entity/Menu.java
@@ -1,64 +1,128 @@
 package com.delivious.backend.domain.menu.entity;
-
-
 import com.delivious.backend.global.common.BaseEntity;
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Builder
-@AllArgsConstructor
-@Table(name = "menu")
+@Table(name = "menus")
 public class Menu extends BaseEntity {
 
-    @NotNull
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column( nullable = false, length = 50)
-    private UUID menu_id;
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)", name = "menu_id")
+    private UUID menuId;
+
+    @NotNull
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @NotNull
+    private Float price;
+
+    @NotNull
+    @Column(name = "menu_image")
+    private String menuImage;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="category_id")
+    @JoinColumn(name = "category_id")
+    @JsonBackReference // 순환참조 방지
     private Category category;
 
-    // 여기서 img_id or img_url
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="img_id")
-    private Img img;
+    @Transient //categoryId 제외
+    private UUID categoryId;
 
-
-//    @Column( nullable = false, length = 50)
-//    private Long store_num;
-
-    @Column( nullable = false, length = 50)
-    private String menu_name;
-
-    // Long -> int
-    @Column( nullable = false, length = 15)
-    private int menu_price;
-
-    @Column(nullable = false)
+    @NotNull
     private String temperature;
 
-    // size를 db에 어떤식으로 넣어야 할지
     @Enumerated(EnumType.STRING)
     private Size size;
 
-    @Column( nullable = false, length = 50)
+    @NotNull
     private String description;
 
-    public void update(Img img, String menu_name, int menu_price, String temperature, String description) {
-        this.img = img;
-        this.menu_name = menu_name;
-        this.menu_price = menu_price;
+    public Menu() {
+
+    }
+
+    public Menu(String menuName, Float price, String menuImage, String temperature, Size size, String description) {
+        this.menuName = menuName;
+        this.price = price;
+        this.menuImage = menuImage;
         this.temperature = temperature;
+        this.size = size;
+        this.description = description;
+    }
+
+    public UUID getMenuId() {
+        return menuId;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getMenuImage() {
+        return menuImage;
+    }
+
+    public void setMenuImage(String menuImage) {
+        this.menuImage = menuImage;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public UUID getCategoryId() {
+        return getCategory().getCategoryId();
+    }
+
+    public void setCategoryId(UUID categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
