26dc946dc6ae0bfbd4dde5843312caaf9be68532
hyelin <k1h2fls@gmail.com>
2022-06-27 17:12:41 +0900

    feat: add menu api

--- backend/src/main/java/com/delivious/backend/domain/menu/exception/MenuAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package com.delivious.backend.domain.menu.exception;
-
-public class MenuAlreadyExists extends RuntimeException{
-    public MenuAlreadyExists(String msg) {
-        super(msg);
-    }
-}
