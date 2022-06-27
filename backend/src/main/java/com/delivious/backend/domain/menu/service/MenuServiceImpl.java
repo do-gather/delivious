9bf11f52a16cb28aad715e7c46e9cce5341a71bf
hyelin <k1h2fls@gmail.com>
2022-06-27 17:12:41 +0900

    feat: add menu api

--- backend/src/main/java/com/delivious/backend/domain/menu/service/MenuServiceImpl.java
+++ backend/src/main/java/com/delivious/backend/domain/menu/service/MenuServiceImpl.java
@@ -1,9 +1,6 @@
 package com.delivious.backend.domain.menu.service;
 
-import com.delivious.backend.domain.category.entity.Category;
-import com.delivious.backend.domain.category.repository.CategoryRepository;
 import com.delivious.backend.domain.menu.dto.MenuRequest;
-import com.delivious.backend.domain.menu.dto.MenuResponse;
 import com.delivious.backend.domain.menu.entity.Menu;
 import com.delivious.backend.domain.menu.exception.MenuNotFoundException;
 import com.delivious.backend.domain.menu.repository.MenuRepository;
@@ -11,27 +8,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MenuServiceImpl implements MenuService {
     private final MenuRepository menuRepository;
-    private final CategoryRepository categoryRepository;
 
     // 메뉴 추가
     @Override
     @Transactional
     public void createMenu(MenuRequest menuRequest) {
-//        Optional<Category> category = categoryRepository.findById(menuRequest.getCategory().getCategoryId());
-//        if (!category.isPresent()) {
-//            throw new EntityNotFoundException(
-//                    "카테고리가 존재하지 않습니다.");
-//        }
         Menu menu = menuRequest.toEntity();
         menuRepository.save(menu);
     }
@@ -49,11 +37,9 @@ public class MenuServiceImpl implements MenuService {
         return menuRepository.findById(menuId).orElseThrow(MenuNotFoundException::new);
     }
 
-//   // 메뉴 리스트 불러오기(by categroyId)
-//    @Override
-//    @Transactional(readOnly = true)
-//    public List<MenuResponse> findAllByCategoryId(UUID categoryId) {
-//        return menuRepository.findByCategoryId(categoryId).stream().map(MenuResponse::new).collect(Collectors.toList());
+   // 메뉴 리스트 불러오기(by categroyId)
+//    public List<Menu> getMenuByCategoryId(UUID categoryId) {
+//        return menuRepository.getMenuByCategoryId(categoryId);
 //    }
 
     // 메뉴수정
