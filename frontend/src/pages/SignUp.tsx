106e1cd2f66923bbb450e27a2aa278717ba637e5
jel-lambda <sheee7@naver.com>
2022-07-04 22:21:23 +0900

    feat: add api for login and sign up

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import BasicButton from '../components/BasicButton';
 import InputBox from '../components/InputBox';
 import CheckBox from '../components/CheckBox';
 import UserIcon from '../images/UserIcon';
 import PasswordIcon from '../images/PasswordIcon';
+import AuthService from '../services/AuthService';
+
+interface UserInfo {
+  username: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  phoneNum: string;
+  year: string;
+  month: string;
+  date: string;
+  type: string;
+  storeName: string;
+}
 
 /**
  * '/sign-up'로 연결되는 회원가입 페이지
  */
 
 export default function SignUp() {
+  const navigate = useNavigate();
   const [admin, setAdmin] = useState(false);
-  const [userInfo, setUserInfo] = useState({
+  const [userInfo, setUserInfo] = useState<UserInfo>({
     username: '',
     password: '',
     passwordConfirm: '',
@@ -20,9 +36,49 @@ export default function SignUp() {
     year: '',
     month: '',
     date: '',
-    type: 'USER',
+    type: 'user',
     storeName: '',
   });
+
+  const sendUserInfo = (e: any) => {
+    e.preventDefault();
+    if (userInfo.password !== userInfo.passwordConfirm) {
+      // eslint-disable-next-line no-alert
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+    const data = {
+      username: userInfo.username,
+      password: userInfo.password,
+      name: userInfo.name,
+      phoneNum: userInfo.phoneNum,
+      birth: new Date(Number(userInfo.year), Number(userInfo.month), Number(userInfo.date)).toISOString().slice(0, 10),
+      type: userInfo.type,
+    };
+
+    AuthService.register(data).then((res: any) => {
+      if (res.status === 201) {
+        if (userInfo.type === 'admin') {
+          AuthService.login(userInfo.username, userInfo.password).then((response: any) => {
+            if (response.status === 200) {
+              const store = {
+                userId: res.data.userId,
+                storeName: userInfo.storeName,
+              };
+              AuthService.store(store, response.data.token).then((storeres: any) => {
+                if (storeres.status === 200) {
+                  navigate('/login');
+                }
+              });
+            }
+          });
+        } else {
+          navigate('/login');
+        }
+      }
+      return res;
+    });
+  };
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
@@ -30,7 +86,7 @@ export default function SignUp() {
   };
 
   const changeAdmin = (checked: boolean) => {
-    setUserInfo({ ...userInfo, type: checked ? 'ADMIN' : 'USER' });
+    setUserInfo({ ...userInfo, type: checked ? 'admin' : 'user' });
     setAdmin(checked);
   };
 
@@ -98,13 +154,7 @@ export default function SignUp() {
           )}
         </div>
         <div>
-          <BasicButton
-            buttonName="Sign Up"
-            onClick={() => {
-              console.log('signup');
-            }}
-            height="h-14"
-          />
+          <BasicButton buttonName="Sign Up" onClick={sendUserInfo} height="h-14" />
         </div>
       </div>
     </div>
