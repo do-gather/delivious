20fe4aeac020febf9589a33d1504a6208fc8d7b2
jel-lambda <sheee7@naver.com>
2022-07-05 01:13:46 +0900

    feat: check token expiration

---
+++ frontend/src/utils/checkToken.ts
@@ -0,0 +1,23 @@
+import { decodeJwt } from './jwtUtils';
+
+export default function checkToken() {
+  const access = localStorage.getItem('adminToken');
+
+  if (access) {
+    // 현재 시각
+    const currentTime = new Date().getTime() / 1000;
+
+    // access 토큰 만료 시각
+    const accessExpireAt = decodeJwt(access).exp;
+
+    // access 토큰 만료 시간을 임박/초과했을 경우
+    if (accessExpireAt - currentTime <= 0) {
+      // eslint-disable-next-line no-alert
+      alert('로그인이 만료됐어요. 다시 로그인해 주세요.');
+      window.localStorage.removeItem('adminToken');
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
