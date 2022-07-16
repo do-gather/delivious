ee5b303d9291ab6e8f2ba87f55a9cd8109dc1eab
jel-lambda <sheee7@naver.com>
2022-07-17 02:07:27 +0900

    feat: make auth route work with user type

--- frontend/src/utils/AuthRoute.tsx
+++ frontend/src/utils/AuthRoute.tsx
@@ -4,27 +4,36 @@ import React, { ReactElement, useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import checkToken from './checkToken';
 import useAuth from './store';
+import { dts } from './types';
 
-interface PrivateRouteProps {
-  children?: ReactElement; // Router.tsx에서 PrivateRoute가 감싸고 있는 Componet Element
-  authentication: boolean; // true :인증을 반드시 해야하만 접속가능, false : 인증을 반디스 안해야만 접속 가능
-}
+/**
+ * 라우터를 감싸는 권한 확인을 위한 컴포넌트
+ * @param authentication
+ */
 
-export function AuthRoute({ authentication }: PrivateRouteProps): React.ReactElement | null {
+export function AuthRoute(): ReactElement | null {
   const { removeAccess } = useAuth();
   const navigate = useNavigate();
+  const url = window.location.pathname;
 
   useEffect(() => {
     (async () => {
-      const token = checkToken();
-      if (!token && authentication) {
+      const token: dts.tokenDto = checkToken();
+      if (!token.activation && url.includes('/admin')) {
         navigate('/login');
         alert('로그인이 필요합니다.');
+        removeAccess();
+      } else if (!token.activation) {
+        navigate('/mypage/login');
+        alert('로그인이 필요합니다.');
+        removeAccess();
+      } else if (token.activation && token.type === 'user' && url.includes('/admin')) {
+        navigate('/admin');
+        alert('관리자만 접근 가능합니다.');
         removeAccess();
       }
     })();
   }, []);
-
   return <Outlet />;
 }
 
