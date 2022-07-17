4a285113a0e438a946bf7929c0cd6510c12a58df
jel-lambda <sheee7@naver.com>
2022-07-17 17:49:07 +0900

    fix: fix build error cause by type

--- frontend/src/components/MyOrder.tsx
+++ frontend/src/components/MyOrder.tsx
@@ -24,18 +24,16 @@ export default function MyOrder({
     date: '',
     image: '',
   },
-  type = 'display',
 }: Props) {
   return (
     <div className="flex justify-between whitespace-nowrap text-center items-center text-base">
-      
-      <div className='pl-10'>{props.date}</div>
-        <div className="flex justify-between py-6 whitespace-nowrap text-center items-center text-base">
-          {props.name} <CountMenu orderList={constants.ORDER}/>
-        </div>
+      <div className="pl-10">{props.date}</div>
+      <div className="flex justify-between py-6 whitespace-nowrap text-center items-center text-base">
+        {props.name} <CountMenu orderList={constants.ORDER} />
+      </div>
 
       <div className="flex text-center pr-10">
-      <LinkButton color="#435ca5" name="상세보기" link="/mypage/orderhistory" />
+        <LinkButton color="#435ca5" name="상세보기" link="/mypage/orderhistory" />
       </div>
     </div>
   );
