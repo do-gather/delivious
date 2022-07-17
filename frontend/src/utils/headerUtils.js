308ddf7ea0e631e0f37f49e9f1904da2f5b1f056
jel-lambda <sheee7@naver.com>
2022-07-17 19:27:31 +0900

    fix: delete duplicated header utils file

--- frontend/src/utils/headerUtils.js
+++
@@ -1,9 +0,0 @@
-function authHeader(access) {
-  return { Authorization: `Bearer ${access}`, 'Content-Type': `application/json` };
-}
-
-function contentTypeJsonHeader() {
-  return { 'Content-Type': `application/json` };
-}
-
-module.exports = { authHeader, contentTypeJsonHeader };
